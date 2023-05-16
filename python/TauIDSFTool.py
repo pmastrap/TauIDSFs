4949d167f027da94624b403a84622930625445d1
Paola Mastrapasqua <paolamastrapasqua2014@gmail.com>
2023-05-16 11:36:16 +0200

    add pT-DM binned

--- python/TauIDSFTool.py
+++ python/TauIDSFTool.py
@@ -3,9 +3,8 @@
 from __future__ import print_function
 import os
 from math import sqrt
-import ctypes
 if 'CMSSW_BASE' in os.environ: # assume CMSSW environment
-  from TauPOG.TauIDSFs.helpers import ensureTFile, extractTH1, extractTF1DMandPT
+  from TauPOG.TauIDSFs.helpers import ensureTFile, extractTH1
   datapath = os.path.join(os.environ.get('CMSSW_BASE',""),"src/TauPOG/TauIDSFs/data")
 else:
   from helpers import ensureTFile, extractTH1
@@ -18,11 +17,7 @@ campaigns  = [
 
 class TauIDSFTool:
     
-<<<<<<< HEAD
     def __init__(self, year, id, wp='Tight', dm=False, pTdm=False, emb=False,
-=======
-    def __init__(self, year, id, wp='Medium', wp_vsele='VVLoose', dm=False, ptdm=True, emb=False, highpT=False,
->>>>>>> 25ca8b42ae4171a2fb72a2a50e808afac8bb3f26
                  otherVSlepWP=False, path=datapath, verbose=False):
         """Choose the IDs and WPs for SFs. For available tau IDs and WPs, check
         https://cms-nanoaod-integration.web.cern.ch/integration/master-102X/mc102X_doc.html#Tau
@@ -38,57 +33,8 @@ class TauIDSFTool:
         self.extraUnc = None
         self.filename = None
         
-<<<<<<< HEAD
-        if id in ['MVAoldDM2017v2','DeepTau2017v2p1VSjet', 'DeepTau2018v2p5VSjet']:
-          if dm and pTdm==False : # DM-dependent SFs
-=======
         if id in ['MVAoldDM2017v2','DeepTau2017v2p1VSjet']:
-          if highpT: # pT-dependent SFs from W*->taunu events
-            allowed_wp=['Loose','Medium','Tight','VTight']
-            allowed_wp_vsele=['VVLoose','Tight']
-            if id != 'DeepTau2017v2p1VSjet': raise IOError("Scale factors not available for ID '%s'!"%id)
-            if wp not in allowed_wp or wp_vsele not in allowed_wp_vsele:
-              raise IOError("Scale factors not available for this combination of WPs! Allowed WPs for VSjet are [%s]. Allowed WPs for VSele are [%s]"%(', '.join(allowed_wp),', '.join(allowed_wp_vsele)))
-            if emb: raise IOError("Scale factors for embedded samples not available in this format! Use either pT-binned or DM-binned SFs.")
-            fname = os.path.join(path,"TauID_SF_Highpt_%s_VSjet%s_VSele%s_Mar07.root" %(id, wp, wp_vsele))
-            file = ensureTFile(fname,verbose=verbose)
-            year_=year
-            if year_.startswith('UL'): year_=year_[2:]
-            self.func         = { }
-            self.func[None]   = file.Get("DMinclusive_%s"%(year_))
-            self.func['syst_alleras']   = file.Get("DMinclusive_%s_syst_alleras"%(year_))
-            self.func['syst_oneera']   = file.Get("DMinclusive_%s_syst_%s"%(year_,year_))
-            file.Close()
-            fname_extrap = os.path.join(path,"TauID_SF_HighptExtrap_%s_Mar07.root" %(id))
-            file_extrap = ensureTFile(fname_extrap,verbose=verbose)
-            self.func['syst_extrap']   = file_extrap.Get("uncert_func_%sVSjet_%sVSe"%(wp,wp_vsele))
-            file_extrap.Close()
-
-
-          elif ptdm: # DM-dependent SFs with pT-dependence fitted
-            self.DMs        = [0,1,10] if 'oldDM' in id else [0,1,10,11]
-            allowed_wp=['Loose','Medium','Tight','VTight']
-            allowed_wp_vsele=['VVLoose','Tight']
-            if id != 'DeepTau2017v2p1VSjet': raise IOError("Scale factors not available for ID '%s'!"%id)
-            if wp not in allowed_wp or wp_vsele not in allowed_wp_vsele:
-              raise IOError("Scale factors not available for this combination of WPs! Allowed WPs for VSjet are [%s]. Allowed WPs for VSele are [%s]"%(', '.join(allowed_wp),', '.join(allowed_wp_vsele)))
-            if emb: raise IOError("Scale factors for embedded samples not available in this format! Use either pT-binned or DM-binned SFs.")
-            fname = os.path.join(path,"TauID_SF_dm_%s_VSjet%s_VSele%s_Mar07.root" %(id, wp, wp_vsele))
-            file = ensureTFile(fname,verbose=verbose)
-            year_=year
-            if year_.startswith('UL'): year_=year_[2:]
-            uncerts=['uncert0','uncert1','syst_alleras','syst_%s' % year_]
-
-            self.funcs_dm0  = extractTF1DMandPT(file,'DM0_%s_fit' % year_,uncerts=uncerts+['syst_dm0_%s' % year_])
-            self.funcs_dm1  = extractTF1DMandPT(file,'DM1_%s_fit' % year_,uncerts=uncerts+['syst_dm1_%s' % year_])
-            self.funcs_dm10 = extractTF1DMandPT(file,'DM10_%s_fit' % year_,uncerts=uncerts+['syst_dm10_%s' % year_])
-            self.funcs_dm11 = extractTF1DMandPT(file,'DM11_%s_fit' % year_,uncerts=uncerts+['syst_dm11_%s' % year_])
-
-            self.getSFvsPT  = self.disabled
-            self.getSFvsDM  = self.disabled
-            self.getSFvsEta = self.disabled
-          elif dm: # DM-dependent SFs
->>>>>>> 25ca8b42ae4171a2fb72a2a50e808afac8bb3f26
+          if dm and pTdm==False : # DM-dependent SFs
             if emb:
               if 'oldDM' in id:
                 raise IOError("Scale factors for embedded samples not available for ID '%s'!"%id)
@@ -116,7 +62,7 @@ class TauIDSFTool:
             file = ensureTFile(fname,verbose=verbose)
             self.func         = { }
             dms = [0,1,10,11]
-            for idm in dms: 
+            for idm in dms:
                 self.func['Nom_DM'+str(idm)]   = file.Get("%s_DM%s_cent"%(wp,idm))
                 self.func['Up_DM'+str(idm)]   = file.Get("%s_DM%s_up"%(wp,idm))
                 self.func['Down_DM'+str(idm)] = file.Get("%s_DM%s_down"%(wp,idm))
@@ -126,7 +72,6 @@ class TauIDSFTool:
             self.getSFvsEta = self.disabled
             self.getSFvsPT  = self.disabled
           #################################
-
 
           else: # pT-dependent SFs
             if emb:
@@ -162,10 +107,10 @@ class TauIDSFTool:
             self.genmatches = [1,3] if any(s in id.lower() for s in ['ele','vse']) else [2,4]
             self.getSFvsPT  = self.disabled
             self.getSFvsDM  = self.disabled
-            self.getSFvsPTDM = self.disabled
         else:
           raise IOError("Did not recognize tau ID '%s'!"%id)
 
+    # my addition
     def getSFvsPTDM(self, idm, pt, genmatch=5, unc=None):
         """Get tau ID SF vs. tau pT divided in DMs"""
         if genmatch==5:
@@ -174,10 +119,10 @@ class TauIDSFTool:
           else:
              return self.func[unc+"_DM"+str(idm)].Eval(pt)
         return 1.0
-
+    ############
     
-    def getSFvsPT(self, dm , pt, genmatch, unc=None):
-        """Get tau ID SF vs. tau pT divided in DMs"""
+    def getSFvsPT(self, pt, genmatch=5, unc=None):
+        """Get tau ID SF vs. tau pT."""
         if genmatch==5:
           if self.extraUnc:
             sf       = self.func[None].Eval(pt)
@@ -197,42 +142,6 @@ class TauIDSFTool:
           return self.func[unc].Eval(pt)
         elif unc=='All':
           return 1.0, 1.0, 1.0
-        return 1.0
-
-    def getHighPTSFvsPT(self, pt, genmatch=5, unc=None):
-        """Get High pT tau ID SF vs. tau pT."""
-        if genmatch==5:
-          x=ctypes.c_double(0.)
-          y=ctypes.c_double(0.)
-          # we only measured for 2 pT bins 100-200 and > 200 so return 1 of 2 values depending on whether pt is less than 200 or not
-          self.func[None].GetPoint(0 if pt<200 else 1, x,y)
-          sf=float(y.value)
-
-          if not unc: return sf
-
-          if 'stat_bin1' in unc and pt<200:
-           # we define the stat error as the statistical error summed in quadrature with the systematic error that is uncorrelated by era
-           # in principle these could be taken as seperate uncertainties but the effect of correlating the systematic part by pT bin will be negligible overall 
-           if 'up' in unc:  sf+=sqrt(self.func[None].GetErrorY(0)**2 + self.func['syst_oneera'].GetErrorY(0)**2)
-           if 'down' in unc:  sf-=sqrt(self.func[None].GetErrorY(0)**2 + self.func['syst_oneera'].GetErrorY(0)**2)
-          if 'stat_bin2' in unc and pt>=200:
-           # we define the stat error as the statistical error summed in quadrature with the systematic error that is uncorrelated by era
-           # in principle these could be taken as seperate uncertainties but the effect of correlating the systematic part by pT bin will be negligible overall 
-           if 'up' in unc:  sf+=sqrt(self.func[None].GetErrorY(1)**2 + self.func['syst_oneera'].GetErrorY(1)**2)
-           if 'down' in unc:  sf-=sqrt(self.func[None].GetErrorY(1)**2 + self.func['syst_oneera'].GetErrorY(1)**2)
-          if 'stat' in unc and 'bin' not in unc:
-           # we define the stat error as the statistical error summed in quadrature with the systematic error that is uncorrelated by era
-           # in principle these could be taken as seperate uncertainties but the effect of correlating the systematic part by pT bin will be negligible overall 
-           if 'up' in unc:  sf+=sqrt(self.func[None].GetErrorY(0 if pt<200 else 1)**2 + self.func['syst_oneera'].GetErrorY(0 if pt<200 else 1)**2)
-           if 'down' in unc:  sf-=sqrt(self.func[None].GetErrorY(0 if pt<200 else 1)**2 + self.func['syst_oneera'].GetErrorY(0 if pt<200 else 1)**2)
-          if 'syst' in unc:
-           if 'up' in unc:  sf+=self.func['syst_alleras'].GetErrorY(0 if pt<200 else 1)
-           if 'down' in unc:  sf-=self.func['syst_alleras'].GetErrorY(0 if pt<200 else 1)
-          if 'extrap' in unc:
-            if 'up' in unc:  sf*=self.func['syst_extrap'].Eval(pt)
-            if 'down' in unc:  sf*=(2.-self.func['syst_extrap'].Eval(pt))
-
-          return sf
         return 1.0
     
     def getSFvsDM(self, pt, dm, genmatch=5, unc=None):
@@ -254,23 +163,7 @@ class TauIDSFTool:
         elif unc=='All':
           return 1.0, 1.0, 1.0
         return 1.0
-   
-    def getSFvsDMandPT(self, pt, dm, genmatch=5, unc=None):
-        """Get tau ID SF vs. tau DM with pT dependence fitted"""
-        if genmatch==5 and dm in self.DMs:
-
-          # get correct functions depending on DM
-          if dm==0: funcs = self.funcs_dm0
-          elif 0<dm<=2: funcs = self.funcs_dm1
-          elif dm==10: funcs = self.funcs_dm10
-          elif dm==11: funcs = self.funcs_dm11
-          
-          if not unc: sf=funcs['nom'].Eval(max(min(pt,140.),20.))
-          else: sf=funcs[unc].Eval(max(min(pt,140.),20.))
-          return sf
-        else:
-          return 1.0
- 
+    
     def getSFvsEta(self, eta, genmatch, unc=None):
         """Get tau ID SF vs. tau eta."""
         eta = abs(eta)
